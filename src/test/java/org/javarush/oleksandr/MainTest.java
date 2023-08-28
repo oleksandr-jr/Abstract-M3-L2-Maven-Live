1bc235757ef38ea3b9a9ab8c5cb813b743205389
Oleksandr <oleksandr.jr@gmail.com>
2023-08-28 18:40:19 +0200

    Init

---
+++ src/test/java/org/javarush/oleksandr/MainTest.java
@@ -0,0 +1,13 @@
+package org.javarush.oleksandr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(1, 1);
+    }
+}
---
+++ src/test/java/org/javarush/oleksandr/StrangeTest.java
@@ -0,0 +1,9 @@
+package org.javarush.oleksandr;
+
+public class StrangeTest {
+
+    @org.junit.jupiter.api.Test
+    void test() {
+
+    }
+}
